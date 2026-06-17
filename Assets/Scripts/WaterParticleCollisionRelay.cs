a343a8e1246812689a37fd4165424c31d46ce0bb
LazyRetroDev <giovannicsena@gmail.com>
2026-06-17 19:44:54 -0300

    Detect raccoons directly from live water particles

--- Assets/Scripts/WaterParticleCollisionRelay.cs
+++ Assets/Scripts/WaterParticleCollisionRelay.cs
@@ -3,11 +3,87 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))]
 public class WaterParticleCollisionRelay : MonoBehaviour
 {
+    [Tooltip("Extra radius around each visible water particle used to detect water-reactive enemies.")]
+    public float particleHitRadius = 0.35f;
+
+    [Tooltip("Layers containing enemies that can react to water.")]
+    public LayerMask waterReactiveLayers = ~0;
+
+    private const int InitialParticleCapacity = 256;
+    private const int ColliderBufferSize = 16;
+
+    private ParticleSystem waterParticles;
+    private ParticleSystem.Particle[] particleBuffer;
+    private readonly Collider[] colliderBuffer = new Collider[ColliderBufferSize];
+
+    void Awake()
+    {
+        waterParticles = GetComponent<ParticleSystem>();
+        particleBuffer = new ParticleSystem.Particle[InitialParticleCapacity];
+    }
+
+    void Update()
+    {
+        DetectRaccoonsInsideWaterParticles();
+    }
+
     void OnParticleCollision(GameObject other)
     {
-        if (other == null) return;
+        NotifyRaccoon(other);
+    }
 
-        RaccoonBehavior raccoon = other.GetComponentInParent<RaccoonBehavior>();
+    void DetectRaccoonsInsideWaterParticles()
+    {
+        if (waterParticles == null || !waterParticles.isPlaying)
+            return;
+
+        int livingParticleCount = waterParticles.particleCount;
+        if (livingParticleCount <= 0)
+            return;
+
+        if (particleBuffer.Length < livingParticleCount)
+            particleBuffer = new ParticleSystem.Particle[Mathf.NextPowerOfTwo(livingParticleCount)];
+
+        int particleCount = waterParticles.GetParticles(particleBuffer);
+        ParticleSystem.MainModule main = waterParticles.main;
+
+        for (int i = 0; i < particleCount; i++)
+        {
+            Vector3 particlePosition = GetWorldPosition(particleBuffer[i], main);
+            float radius = particleHitRadius + particleBuffer[i].GetCurrentSize(waterParticles) * 0.5f;
+            int hitCount = Physics.OverlapSphereNonAlloc(
+                particlePosition,
+                radius,
+                colliderBuffer,
+                waterReactiveLayers,
+                QueryTriggerInteraction.Collide);
+
+            for (int hitIndex = 0; hitIndex < hitCount; hitIndex++)
+                NotifyRaccoon(colliderBuffer[hitIndex].gameObject);
+        }
+    }
+
+    Vector3 GetWorldPosition(ParticleSystem.Particle particle, ParticleSystem.MainModule main)
+    {
+        switch (main.simulationSpace)
+        {
+            case ParticleSystemSimulationSpace.Local:
+                return transform.TransformPoint(particle.position);
+            case ParticleSystemSimulationSpace.Custom:
+                return main.customSimulationSpace != null
+                    ? main.customSimulationSpace.TransformPoint(particle.position)
+                    : particle.position;
+            default:
+                return particle.position;
+        }
+    }
+
+    void NotifyRaccoon(GameObject hitObject)
+    {
+        if (hitObject == null)
+            return;
+
+        RaccoonBehavior raccoon = hitObject.GetComponentInParent<RaccoonBehavior>();
         if (raccoon != null)
             raccoon.ReceiveWaterHit(transform.position);
     }
